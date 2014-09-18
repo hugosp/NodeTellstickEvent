b7a47ceb1829a60cb1d2e0f28e18ee9708fd61b1
hugosp <stefan.sundell@gmail.com>
2014-09-18 12:40:11 +0200

    clean up

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 var net = require('net');
  
-// Set up a connection to the TelldusEvents socket
+// connect to socket
 var conn = net.createConnection('/tmp/TelldusEvents');
- 
-// Set the encoding so that you get data that is actually readable by humans
+
 conn.setEncoding('utf-8');
  
 //Eventlistener on connection
 conn.on('connect' , function () {
         console.log("Connected");
 });
- 
  
 //Eventlistener on data recieved
 conn.on('data', function(data) {
